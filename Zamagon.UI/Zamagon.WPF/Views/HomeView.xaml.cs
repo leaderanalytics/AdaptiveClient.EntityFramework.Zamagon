c0b9ed57c0d6cfb95f5c0325d0c6d0d882970e32
Sam <sam.wheat@outlook.com>
2018-06-15 16:52:06 -0700

    Progress on WPF app

--- Zamagon.UI/Zamagon.WPF/Views/HomeView.xaml.cs
+++ Zamagon.UI/Zamagon.WPF/Views/HomeView.xaml.cs
@@ -29,5 +29,10 @@ namespace Zamagon.WPF.Views
         {
             base.IsSelected_Changed(endPoints);
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            string name = ((CheckBox)sender).Tag.ToString();
+        }
     }
 }
